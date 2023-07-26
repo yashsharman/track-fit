d37290bff72eaaf21373958c849ab7a2aca9aa5b
yashsharman <85861411+yashsharman@users.noreply.github.com>
2023-07-26 18:11:44 +0530

    added recordExercise component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
   // State to store the search query
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   // State to store the search results
   const [searchResults, setSearchResults] = useState([]);
@@ -10,41 +10,127 @@ function App() {
   // State to track the active main category for displaying subcategories
   const [activeCategory, setActiveCategory] = useState(null);
 
-  const data = [
-    {
-      category: 'Abs',
-      subcategories: ['Ab-Wheel Rollout', 'Cable Crunch', 'Crunch', 'Crunch Machine', 'Decline Crunch', 'Dragon Flag', 'Hanging Knee Raise', 'Hanging Leg Raise', 'Plank', 'Side Plank'],
-    },
-    {
-      category: 'Back',
-      subcategories: ['Barbell Row', 'Barbell Shrug', 'Chin Up', 'Deadlift', 'Dumbell Row', 'Good Morning', 'Hammer Strength Row', 'Lat Pulldown', 'Machine Shrug', 'Neutral Chin Up', 'Pendlay Row', 'Pull Up', 'Rack Pull', 'Seated Cable Row', 'Straight-Arm Cable Pushdown', 'T-Bar Row'],
-    },
-    {
-      category: 'Biceps',
-      subcategories: ['Barbell Curl', 'Cable Curl', 'Dumbbell Curl', 'Dumbbell Concentration Curl', 'Dumbbell Hammer Curl', 'Dumbbell Preacher Curl', 'EZ-Bar Curl', 'EZ-Bar Preacher Curl', 'Seated Incline Dumbbell Curl', 'Seated Machine Curl'],
-    },
-    {
-      category: 'Cardio',
-      subcategories: ['Running(Outdoor)', 'Running(Treadmill)', 'Cycling', 'Swimming', 'Walking', 'Elliptical Trainer', 'Rowing Machine', 'Stationary Bike'],
-    },
-    {
-      category: 'Chest',
-      subcategories: ['Cable Crossover', 'Decline Barbell Bench Press', 'Decline Hammer Strength Chest Press', 'Flat Barbell Bench Press', 'Flat Dumbbell Bench Press', 'Flat Dumbbell Fly', 'Incline Barbell Bench Press', 'Incline Dumbbell Bench Press', 'Incline Dumbbell Fly', 'Incline Hammer Strength Chest Press', 'Seated Machine Fly'],
-    },
-    {
-      category: 'Legs',
-      subcategories: ['Barbell Calf Raise', 'Barbell Front Squat', 'Barbell Glute Bridge', 'Barbell Squat', 'Donkey Calf Raise', 'Glute-Ham Raise', 'Leg Extention Machine', 'Leg Press', 'Lying Leg Curl Machine', 'Romanian Deadlift', 'Seated Calf Raise Machine', 'Seated Leg Curl Machine', 'Standing Calf Raise Machine', 'Stiff-Legged Deadlift', 'Sumo Deadlift'],
-    },
-    {
-      category: 'Shoulders',
-      subcategories: ['Arnold Dumbbell Press', 'Behind The Neck Barbell Press', 'Cable Face Pull', 'Front Dumbbell Raise', 'Hammer Strength Shoulder Press', 'Lateral Dumbbell Raise', 'Lateral Machine Raise', 'Log Press', 'One-Arm Standing Dumbbell Press', 'Overhead Press', 'Rear Delt Dumbbell Raise', 'Rear Delt Machine Fly', 'Seated Dumbbell Lateral Raise', 'Seated Dumbbell Press', 'Smith Machine Overhead Press'],
-    },
-    {
-      category: 'Triceps',
-      subcategories: ['Cable Overhead Triceps Extention', 'Close Grip Barbell Bench Press', 'Dumbbell Overhead Triceps Extention', 'EZ-Bar Skullcrusher', 'Lying Triceps Extention', 'Parallel Bar Triceps Dip', 'Ring Dip', 'Rope Push Down', 'Smith Machine Close Grip Bench Press', 'V-Bar Push Down'],
-    },
-  ];
+  const data = {
+    Abs: [
+      "Ab-Wheel Rollout",
+      "Cable Crunch",
+      "Crunch",
+      "Crunch Machine",
+      "Decline Crunch",
+      "Dragon Flag",
+      "Hanging Knee Raise",
+      "Hanging Leg Raise",
+      "Plank",
+      "Side Plank",
+    ],
 
+    Back: [
+      "Barbell Row",
+      "Barbell Shrug",
+      "Chin Up",
+      "Deadlift",
+      "Dumbell Row",
+      "Good Morning",
+      "Hammer Strength Row",
+      "Lat Pulldown",
+      "Machine Shrug",
+      "Neutral Chin Up",
+      "Pendlay Row",
+      "Pull Up",
+      "Rack Pull",
+      "Seated Cable Row",
+      "Straight-Arm Cable Pushdown",
+      "T-Bar Row",
+    ],
+
+    Biceps: [
+      "Barbell Curl",
+      "Cable Curl",
+      "Dumbbell Curl",
+      "Dumbbell Concentration Curl",
+      "Dumbbell Hammer Curl",
+      "Dumbbell Preacher Curl",
+      "EZ-Bar Curl",
+      "EZ-Bar Preacher Curl",
+      "Seated Incline Dumbbell Curl",
+      "Seated Machine Curl",
+    ],
+
+    Cardio: [
+      "Running(Outdoor)",
+      "Running(Treadmill)",
+      "Cycling",
+      "Swimming",
+      "Walking",
+      "Elliptical Trainer",
+      "Rowing Machine",
+      "Stationary Bike",
+    ],
+
+    Chest: [
+      "Cable Crossover",
+      "Decline Barbell Bench Press",
+      "Decline Hammer Strength Chest Press",
+      "Flat Barbell Bench Press",
+      "Flat Dumbbell Bench Press",
+      "Flat Dumbbell Fly",
+      "Incline Barbell Bench Press",
+      "Incline Dumbbell Bench Press",
+      "Incline Dumbbell Fly",
+      "Incline Hammer Strength Chest Press",
+      "Seated Machine Fly",
+    ],
+
+    Legs: [
+      "Barbell Calf Raise",
+      "Barbell Front Squat",
+      "Barbell Glute Bridge",
+      "Barbell Squat",
+      "Donkey Calf Raise",
+      "Glute-Ham Raise",
+      "Leg Extention Machine",
+      "Leg Press",
+      "Lying Leg Curl Machine",
+      "Romanian Deadlift",
+      "Seated Calf Raise Machine",
+      "Seated Leg Curl Machine",
+      "Standing Calf Raise Machine",
+      "Stiff-Legged Deadlift",
+      "Sumo Deadlift",
+    ],
+
+    Shoulders: [
+      "Arnold Dumbbell Press",
+      "Behind The Neck Barbell Press",
+      "Cable Face Pull",
+      "Front Dumbbell Raise",
+      "Hammer Strength Shoulder Press",
+      "Lateral Dumbbell Raise",
+      "Lateral Machine Raise",
+      "Log Press",
+      "One-Arm Standing Dumbbell Press",
+      "Overhead Press",
+      "Rear Delt Dumbbell Raise",
+      "Rear Delt Machine Fly",
+      "Seated Dumbbell Lateral Raise",
+      "Seated Dumbbell Press",
+      "Smith Machine Overhead Press",
+    ],
+
+    Triceps: [
+      "Cable Overhead Triceps Extention",
+      "Close Grip Barbell Bench Press",
+      "Dumbbell Overhead Triceps Extention",
+      "EZ-Bar Skullcrusher",
+      "Lying Triceps Extention",
+      "Parallel Bar Triceps Dip",
+      "Ring Dip",
+      "Rope Push Down",
+      "Smith Machine Close Grip Bench Press",
+      "V-Bar Push Down",
+    ],
+  };
+console.log(Object.keys(data))
   // Function to handle search input changes
   const handleSearchInputChange = (event) => {
     const query = event.target.value;
@@ -52,14 +138,14 @@ function App() {
 
     // Filter the data based on the search query
     const filteredResults = data.reduce((acc, item) => {
-      const filteredSubcategories = item.subcategories.filter(subcat =>
+      const filteredSubcategories = item.subcategories.filter((subcat) =>
         subcat.toLowerCase().includes(query.toLowerCase())
       );
 
       if (filteredSubcategories.length > 0) {
         acc.push({
           category: item.category,
-          subcategories: filteredSubcategories,
+          filteredSubcategories,
         });
       }
 
@@ -108,10 +194,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,44 +1,44 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// const SearchBar = ({ data, onSearch }) => {
-//   const [query, setQuery] = useState('');
-//   const [selectedCategory, setSelectedCategory] = useState('All');
+const SearchBar = ({ data, onSearch }) => {
+  const [query, setQuery] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
-//   const handleInputChange = (event) => {
-//     setQuery(event.target.value);
-//   };
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
 
-//   const handleCategoryChange = (event) => {
-//     setSelectedCategory(event.target.value);
-//   };
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
-//   const handleSearch = () => {
-//     onSearch(query, selectedCategory);
-//   };
+  const handleSearch = () => {
+    onSearch(query, selectedCategory);
+  };
 
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         placeholder="Search..."
-//         value={query}
-//         onChange={handleInputChange}
-//       />
-//       <select value={selectedCategory} onChange={handleCategoryChange}>
-//         <option value="All">All</option>
-//         <option value="Category1">Abs</option>
-//         <option value="Category2">Back</option>
-//         <option value="Category2">Biceps</option>
-//         <option value="Category2">Cardio</option>
-//         <option value="Category2">Chest</option>
-//         <option value="Category2">Legs</option>
-//         <option value="Category2">Shoulders</option>
-//         <option value="Category2">Triceps</option>
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={handleInputChange}
+      />
+      <select value={selectedCategory} onChange={handleCategoryChange}>
+        <option value="All">All</option>
+        <option value="Category1">Abs</option>
+        <option value="Category2">Back</option>
+        <option value="Category2">Biceps</option>
+        <option value="Category2">Cardio</option>
+        <option value="Category2">Chest</option>
+        <option value="Category2">Legs</option>
+        <option value="Category2">Shoulders</option>
+        <option value="Category2">Triceps</option>
         
-//       </select>
-//       <button onClick={handleSearch}>Search</button>
-//     </div>
-//   );
-// };
+      </select>
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
 
-// export default SearchBar;
+export default SearchBar;
