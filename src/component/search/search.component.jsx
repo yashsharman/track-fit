9bbc6ced47a1fd6a14f1e79ea362e1aad722fe04
Janvi <120112786+janvi25me@users.noreply.github.com>
2023-08-11 17:29:14 +0530

    bootstrap uninstalled

--- src/component/search/search.component.jsx
+++ src/component/search/search.component.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './search.styles.css';
-// import { Search as searchIconBootstrap} from 'react-bootstrap-icons';
-
 
 const Search = ({ data }) => {
   const [searchQuery, setSearchQuery] = useState("");
